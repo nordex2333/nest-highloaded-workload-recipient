7dda47ce80c0bfe87f1e534745f2d6ea1af262a6
nordex2333 <rocky92@inbox.lv>
2025-06-10 10:33:09 +0300

    fix/master code small refactor

--- src/transaction-api/transaction-api.service.ts
+++ src/transaction-api/transaction-api.service.ts
@@ -14,24 +14,14 @@ export class TransactionApiService {
     startDate?: string,
     endDate?: string,
     page = 1,
-    limit = 1000,
+    limit = 100,
   ) {
-    //AK NEW USE IT
     let where: any = startDate || endDate ? {
         createdAt: Between(
             new Date(startDate || 0), 
             new Date(endDate || Date.now())
         )
     } : {};
-
-    // let where: any = {};
-    // if (startDate && endDate) {
-    //   where.createdAt = Between(new Date(startDate), new Date(endDate));
-    // } else if (startDate) {
-    //   where.createdAt = Between(new Date(startDate), new Date());
-    // } else if (endDate) {
-    //   where.createdAt = Between(new Date(0), new Date(endDate));
-    // }
 
     let [items, totalItems] = await this.transactionRepository.findAndCount({
       where,
