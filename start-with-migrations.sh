073f5479189eee18de2551341aeb544afbdfd01a
nordex2333 <rocky92@inbox.lv>
2025-06-11 03:09:42 +0300

    fix/redis-broker-addon migration runner minor adjustments

--- start-with-migrations.sh
+++ start-with-migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running migrations..."
 export RUN_MIGRATIONS=true
-npm run migration:runner
+docker exec -it nestjs-app npm run migration:runner
 echo "Starting application..."
 npm run start:dev
